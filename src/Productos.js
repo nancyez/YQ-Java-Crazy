06b44ba3ce223a491fecd4859df3758203e42d83
Hiramnam <hiramnm14@gmail.com>
2023-07-17 10:32:43 -0600

    Formulario

---
+++ src/Productos.js
@@ -0,0 +1,18 @@
+// Obtenemos todos los botones "Agregar al carrito"
+const botonesCarrito = document.querySelectorAll('.botton-item');
+
+// Iteramos sobre cada botón y agregamos un listener de click
+botonesCarrito.forEach((boton) => {
+  boton.addEventListener('click', () => {
+    // Obtenemos el precio actual del elemento
+    const precioItem = boton.parentNode.querySelector('.precio-item');
+    const precioActual = parseFloat(precioItem.innerText.replace('$', ''));
+
+    // Incrementamos el precio en 100
+    const nuevoPrecio = precioActual + 100;
+
+    // Actualizamos el texto del precio
+    precioItem.innerText = '$' + nuevoPrecio.toFixed(2);
+  });
+});
+
