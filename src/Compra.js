2e969650f67892a639bbc403cad1b5aa218785b5
Hector Cid del Prado <hectorciddelprado@Hectors-MacBook-Pro.local>
2023-07-18 12:39:49 -0600

    Actualizacion registro, compra, productos

--- src/Compra.js
+++ src/Compra.js
@@ -1,185 +1,106 @@
-//Array de objetos 
-/*
-const baseDeDatos = [
-    {
-        id: 1,
-        nombre: 'Mermelada de pera',
-        precio: 100.00,
-        sabor: 'Sabor: pera.',
-        contenido: 'Contenido: 500g.',
-        categoria:'Mermeladas',
-        imagen: '../assets/ImgProductos/Mermelada-de-pera.png'
-    },
-    {
-        id: 2,
-        nombre: 'Mermelada de zarzamora',
-        precio: 100.00,
-        sabor: 'Sabor: zarzamora.',
-        contenido: 'Contenido: 500g.',
-        categoria:'Mermeladas',
-        imagen: '../assets/ImgProductos/Mermelada-de-zarzamora.png'
-    },    
-    {
-        id: 3,
-        nombre: 'Mermelada de ciruela',
-        precio: 100.00,
-        sabor: 'Sabor: ciruela.',
-        contenido: 'Contenido: 500g.',
-        categoria:'Mermeladas',
-        imagen: '../assets/ImgProductos/Mermelada-de-ciruela.png'
-    },
-    {
-        id: 4,
-        nombre: 'Peras en almibar',
-        precio: 150.00,
-        sabor: 'Sabor: pera.',
-        contenido: 'Contenido: 1L.',
-        categoria:'Almibares',
-        imagen: '../assets/ImgProductos/Almibar-de-peras-grande.png'
-    },
-    {
-        id: 5,
-        nombre: 'Peras en almibar',
-        precio: 100.00,
-        sabor: 'Sabor: pera.',
-        contenido: 'Contenido: 500g.',
-        categoria:'Almibares',
-        imagen: '../assets/ImgProductos/Almibar-de-peras.png'
-    },
-    {
-        id: 6,
-        nombre: 'Duraznos en almibar',
-        precio: 150.00,
-        sabor: 'Sabor: durazno.',
-        contenido: 'Contenido: 1000g.',
-        categoria:'Almibares',
-        imagen: '../assets/ImgProductos/Almibar-de-durazno.png'
-    },
-    {
-        id: 7,
-        nombre: 'Licor de pera',
-        precio: 250.00,
-        sabor: 'Sabor: pera.',
-        contenido: 'Contenido: 750ml.',
-        categoria:'Licor',
-        imagen: '../assets/ImgProductos/Licor-de-pera.png'
-    },
-    {
-        id: 8,
-        nombre: 'Licor de zarzamora',
-        precio: 250.00,
-        sabor: 'Sabor: zarzamora.',
-        contenido: 'Contenido: 750ml.',
-        categoria:'Licor',
-        imagen: '../assets/ImgProductos/Licor-de-zarzamora.png'
-    },
-    {
-        id: 9,
-        nombre: 'Licor de ciruela',
-        precio: 250.00,
-        sabor: 'Sabor: ciruela.',
-        contenido: 'Contenido: 750ml.',
-        categoria:'Licor',
-        imagen: '../assets/ImgProductos/Licor-de-ciruela.png'
-    },
-]
+// Obtener elementos del DOM
+const botonesAgregar = document.querySelectorAll('.botton-item');
+const modalCarrito = document.getElementById('carrito-modal');
+const cerrarModal = document.querySelector('.cerrar-modal');
+const carritoItems = document.getElementById('carrito-items');
+const totalCarrito = document.getElementById('total-carrito');
+const comprarBtn = document.getElementById('comprar-btn');
 
-// Funcionalidades Carrito de compras
-const addToCart=document.querySelectorAll('[data-btn-action="add-btn-cart"]');
+// Obtener el carrito almacenado en el Local Storage o crear uno nuevo
+let carrito = JSON.parse(localStorage.getItem('carrito-modal')) || [];
 
-const closeModal=document.querySelector('.jsModalClose');
-
-addToCart.forEach(btn => {
-    
-    btn.addEventListener('click',(event)=>{
-
-        const nameModal=event.target.getAttribute('data-modal');
-
-        const modal=document.querySelector(nameModal);
-
-        //abrimos el modal
-        modal.classList.add('active');
-
-    })
-
+// Event listeners
+botonesAgregar.forEach((boton) => {
+  boton.addEventListener('click', agregarAlCarrito);
 });
 
-//CERRAR EL MODAL
-closeModal.addEventListener('click',(event)=>{
-    event.target.parentNode.parentNode.classList.remove('active');
-})
+cerrarModal.addEventListener('click', cerrarCarritoModal);
+comprarBtn.addEventListener('click', comprar);
 
-//CERRAMOS MODAL CUANDO HACEMOS CLICK FUERA DEL CONTENDINO DEL MODAL
-window.onclick = (event)=>{
-    const modal=document.querySelector('.modal.active');
-
-    if(event.target == modal){
-        modal.classList.remove('active');
-    }
-}*/
-
-// cart 
-//Cart
-let cartIcon = document.querySelector('#cart-icon')
-let cart = document.querySelector('.cart')
-let closeCart = document.querySelector('#close-cart')
-
-// Open Cart
-cartIcon.onclick = () => { cart.classList.add("active");
-};
-// Close Cart
-closeCart.onclick= () => { cart.classList.remove("active");
-};
-
-//Cart Working JS
-if (document.readyState == "loading") { document.addEventListener("DOMContentLoaded", ready);
-} else {
-ready();
+// Función para agregar un producto al carrito
+function agregarAlCarrito(event) {
+  const item = event.target.parentElement;
+  const titulo = item.querySelector('.titulo-item').textContent;
+  const precio = parseFloat(item.querySelector('.precio-item').textContent.replace('$', ''));
+  
+  const nuevoItem = {
+    titulo: titulo,
+    precio: precio,
+    cantidad: 1
+  };
+  
+  carrito.push(nuevoItem);
+  
+  actualizarCarrito();
+  
+  modalCarrito.style.display = 'block';
 }
 
-// Making Function
-function ready(){
-    // Remove Items From Cart
-    var removeCartButtons = document.getElementsByClassName('cart-remove') 
-    console.log(removeCartButtons)
-    for (var i = 0; i < removeCartButtons.length; i++){
-        var button = removeCartButtons[i]
-        button.addEventListener('click', removeCartItem)
-    }
-    //Quantity Changes
-    var quantityInputs = document.getElementsByClassName('cart-quantity')
-    for (var i = 0; i < removeCartButtons.length; i++) {
-        var input = quantityInputs[i];
-        input.addEventListener("change", quantityChanged);
-    }
+// Función para eliminar un producto del carrito
+function eliminarDelCarrito(event) {
+  const item = event.target.parentElement;
+  const indice = getIndexFromElement(item);
+  
+  carrito.splice(indice, 1);
+  
+  actualizarCarrito();
 }
 
-// Remove Items From Cart
-function removeCartItem(event) { 
-    var buttonClicked = event.target   
-    buttonClicked.parenElement.remove()
-    updatetotal();
-
-}
-//Quantity Changes
-
-//Update Total
-function updateTotal(){
-    var cartContent = document.getElementsByClassName("cart-content")[0];
-    var cartBoxes = cartContent.getElementsByClassName("cart-box");
-    var total = 0;
-    for (var i = 0; i < cartBoxes.length; i++) { 
-        var cartBox = cartBoxes[i];
-        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
-        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
-        var price = parseFloat(priceElement.innerText.replace("$", ""));
-        var quantity = quantityElement.value;
-        total = total + (price * quantity);
-
-        document.getElementsByClassName('total-price')[0].innerHTML = "$" + Total;
-
-    }
-
+// Función para cerrar el modal del carrito
+function cerrarCarritoModal() {
+  modalCarrito.style.display = 'none';
 }
 
+// Función para actualizar el carrito
+function actualizarCarrito() {
+  carritoItems.innerHTML = '';
+  
+  carrito.forEach((producto) => {
+    const nuevoItem = document.createElement('div');
+    nuevoItem.classList.add('carrito-item');
+    nuevoItem.innerHTML = `
+      <span>${producto.titulo}</span>
+      <span>$${producto.precio.toFixed(2)}</span>
+      <button class="eliminar-btn">Eliminar</button>
+    `;
+  
+    const botonEliminar = nuevoItem.querySelector('.eliminar-btn');
+    botonEliminar.addEventListener('click', eliminarDelCarrito);
+  
+    carritoItems.appendChild(nuevoItem);
+  });
+  
+  actualizarTotal();
+  
+  // Guardar el carrito en el Local Storage
+  localStorage.setItem('carrito-modal', JSON.stringify(carrito-modal));
+}
 
+// Función para actualizar el total del carrito
+function actualizarTotal() {
+  let total = 0;
+  
+  carrito.forEach((producto) => {
+    total += producto.precio * producto.cantidad;
+  });
+  
+  totalCarrito.textContent = '$' + total.toFixed(2);
+}
+
+// Función para obtener el índice de un elemento en el carrito
+function getIndexFromElement(element) {
+  const carritoItemsArray = Array.from(carritoItems.children);
+  return carritoItemsArray.indexOf(element);
+}
+
+// Función para realizar la compra
+function comprar() {
+  // Lógica para procesar la compra
+  
+  // Reiniciar carrito
+  carrito = [];
+  
+  actualizarCarrito();
+  
+  cerrarCarritoModal();
+}
